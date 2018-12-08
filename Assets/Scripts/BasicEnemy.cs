4a17ff945eea9b6870ac8e70deaab7c412170bea
Unknown <slyguggi@gmail.com>
2018-12-08 23:18:05 +0100

    Fixed BasicEnemy stopping at least by using composite collider
    KNOWN ISSUE : player cannot jump anymore, working on it

--- Assets/Scripts/BasicEnemy.cs
+++ Assets/Scripts/BasicEnemy.cs
@@ -12,10 +12,7 @@ public class BasicEnemy : Enemy
 	[SerializeField] private GameObject jumpCheckPlatForm;
 	[SerializeField] private GameObject groundDetector;
 	[SerializeField] private GameObject frontDetector;
-	[SerializeField] private bool enableJump = false;
 	[SerializeField] private GameObject bubble;
-	[SerializeField] private float distanceCastGround = 0.55f;
-	[SerializeField] private float radiusCastGround = 0.5f;
 
 
 	private enum BasicEnemyStates
@@ -31,54 +28,58 @@ public class BasicEnemy : Enemy
 	private bool wantsToJump = false;
 	private Rigidbody2D myRigidbody2D;
 	private Vector2 previousPos;
-	private Collider2D frontDetectorCollider2D;
 	private Collider2D jumpPositionCollider2D;
 	private Collider2D jumpCheckPlatFormCollider2D;
-	private TilemapCollider2D tilemapCollider2D;
+	private Collider2D groundDetectorCollider2D;
+	private Collider2D frontDetectorCollider2D;
+	private CompositeCollider2D tilemapCollider2D;
 	private Collider2D myCollider;
 
 	#region Inherited methods
 
 	private void Start()
 	{
+		player = GameManager.Instance.player;
 		myRigidbody2D = GetComponent<Rigidbody2D>();
+		myCollider = GetComponent<Collider2D>();
+		jumpPositionCollider2D = jumpPosition.GetComponent<Collider2D>();
+		jumpCheckPlatFormCollider2D = jumpCheckPlatForm.GetComponent<Collider2D>();
+		groundDetectorCollider2D = groundDetector.GetComponent<Collider2D>();
+		frontDetectorCollider2D = frontDetector.GetComponent<Collider2D>();
+		tilemapCollider2D = GameManager.Instance.levelTilemap.GetComponent<CompositeCollider2D>();
+
+		bubble.SetActive(false);
 		float myRotationY = this.transform.rotation.eulerAngles.y;
 		isLookingRight = Mathf.Abs(myRotationY) < 90.0f;
 		//TODO add checkstate somewhere here
-
-		frontDetectorCollider2D = frontDetector.GetComponent<Collider2D>();
-		jumpPositionCollider2D = jumpPosition.GetComponent<Collider2D>();
-		jumpCheckPlatFormCollider2D = jumpCheckPlatForm.GetComponent<Collider2D>();
-		myCollider = GetComponent<Collider2D>();
-
-		bubble.SetActive(false);
-		player = GameManager.Instance.player;
-		tilemapCollider2D = GameManager.Instance.levelTilemap.GetComponent<TilemapCollider2D>();
 	}
 
 	private void FixedUpdate()
 	{
 		switch (myState)
 		{
+			case BasicEnemyStates.Falling:
+			{
+				myRigidbody2D.velocity = myRigidbody2D.velocity * Vector2.up;
+				CheckGround();
+				CheckPlayerPosX();
+			}
+				break;
 			case BasicEnemyStates.Jumping:
 			{
 				CheckFalling();
 			}
 				break;
-			case BasicEnemyStates.Falling:
-			{
-				myRigidbody2D.velocity = myRigidbody2D.velocity * Vector2.up;
-				CheckPlayerPosX();
-				CheckGround();
-			}
-				break;
 			case BasicEnemyStates.Running:
 			{
 				MoveForward();
-				CheckFront();
 				CheckGround();
-				CheckPlayerPosY();
-				CheckForJump();
+				if (myState == BasicEnemyStates.Running)
+				{
+					CheckFront();
+					CheckPlayerPosY();
+					CheckForJump();
+				}
 			}
 				break;
 			case BasicEnemyStates.InBubble:
@@ -116,7 +117,7 @@ public class BasicEnemy : Enemy
 
 	private void CheckFalling()
 	{
-		if (previousPos.y > transform.position.y)
+		if (previousPos.y >= transform.position.y)
 		{
 			myState = BasicEnemyStates.Falling;
 		}
@@ -124,11 +125,7 @@ public class BasicEnemy : Enemy
 
 	private void CheckGround()
 	{
-		Vector2 originCast = groundDetector.transform.position;
-		RaycastHit2D groundHitLeft = Physics2D.Raycast(originCast + Vector2.right * radiusCastGround, Vector2.down, distanceCastGround);
-		RaycastHit2D groundHitRight = Physics2D.Raycast(originCast + Vector2.left * radiusCastGround, Vector2.down, distanceCastGround);
-		if ((groundHitLeft && groundHitLeft.collider.CompareTag("Level")) ||
-		    (groundHitRight && groundHitRight.collider.CompareTag("Level")))
+		if (groundDetectorCollider2D.IsTouching(tilemapCollider2D))
 		{
 			myState = BasicEnemyStates.Running;
 		}
@@ -171,7 +168,7 @@ public class BasicEnemy : Enemy
 	//Checks if jump is possible depending on various conditions
 	private void CheckForJump()
 	{
-		if (enableJump && wantsToJump && !jumpPositionCollider2D.IsTouching(tilemapCollider2D) &&
+		if (wantsToJump && !jumpPositionCollider2D.IsTouching(tilemapCollider2D) &&
 		    jumpCheckPlatFormCollider2D.IsTouching(tilemapCollider2D))
 		{
 			myRigidbody2D.velocity = Vector2.up * jumpSpeed;
@@ -181,7 +178,7 @@ public class BasicEnemy : Enemy
 
 	private void MoveForward()
 	{
-		myRigidbody2D.velocity = (Vector2) transform.right * speed + Vector2.up * myRigidbody2D.velocity.y;
+		myRigidbody2D.velocity = (Vector2) transform.right * speed + Vector2.up * myRigidbody2D.velocity;
 	}
 
 	private void TurnAround()
